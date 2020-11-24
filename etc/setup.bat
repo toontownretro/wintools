969f621bbd8c3142cbc870181933311c246dc78a
Brian Lach <brianlach72@gmail.com>
2020-11-23 20:17:40 -0500

    Add vspecs and setup stuff

---
+++ etc/setup.bat
@@ -0,0 +1,31 @@
+@echo off
+
+echo ------------ WINTOOLS SETUP ------------
+
+set /p PlayerDir=Enter your player directory [%USERPROFILE%\player]:
+if (%PlayerDir% == "") do (
+    set PlayerDir=%USERPROFILE%\player
+)
+
+echo Player dir is: %PlayerDir%
+
+set Projects=wintools dtool panda pandatool direct dmodels otp toontown ttmodels
+
+if not exist %PlayerDir%\vspec do mkdir %PlayerDir%\vspec
+
+for %%x in (%Projects%) do copy %%x.vspec %PlayerDir%\vspec\%%x.vspec
+
+set EnvFile=%PlayerDir%\env.bat
+
+copy Terminal.lnk %PlayerDir%\Terminal.lnk
+
+echo @echo off> %EnvFile%
+echo set PLAYER=%PlayerDir%>> %EnvFile%
+echo set DTOOL=%%PLAYER%%\dtool>> %EnvFile%
+echo set PPREMAKE_CONFIG=%%PLAYER%%\Config.pp>> %EnvFile%
+echo set PYTHONPATH=%%DTOOL%%\src\attach;%%PYTHONPATH%%>> %EnvFile%
+echo set PATH=%%DTOOL%%\src\attach;C:\cygwin64\usr\local\bin;C:\cygin64\bin;%%PATH%%>> %EnvFile%
+echo set CTVSPEC_PATH=%%PLAYER%%\vspec>> %EnvFile%
+echo @call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat">> %EnvFile%
+
+pause
