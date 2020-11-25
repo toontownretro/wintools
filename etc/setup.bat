17fad4073dd98c62e1c5e42f9adf515c2aba74ab
Brian Lach <brianlach72@gmail.com>
2020-11-24 21:45:58 -0500

    remove Cygwin paths from setup.bat

--- etc/setup.bat
+++ etc/setup.bat
@@ -30,7 +30,7 @@ echo set PLAYER=%PlayerDir%>> %EnvFile%
 echo set DTOOL=%%PLAYER%%\dtool>> %EnvFile%
 echo set PPREMAKE_CONFIG=%%PLAYER%%\Config.pp>> %EnvFile%
 echo set PYTHONPATH=%%DTOOL%%\src\attach;%%PYTHONPATH%%>> %EnvFile%
-echo set PATH=%%DTOOL%%\src\attach;C:\cygwin64\usr\local\bin;C:\cygwin64\bin;%%PATH%%>> %EnvFile%
+echo set PATH=%%DTOOL%%\src\attach;%%PATH%%>> %EnvFile%
 echo set CTVSPEC_PATH=%%PLAYER%%\vspec>> %EnvFile%
 echo @call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat">> %EnvFile%
 
